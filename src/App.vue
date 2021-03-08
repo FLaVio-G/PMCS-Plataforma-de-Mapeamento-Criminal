fe65886f5bdc4d9f1f576aaa8b3b36a9c971eee2
katsu26F <flaviogabrielandrade@gmail.com>
2021-03-08 18:07:43 -0300

    segundo comit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="primary"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-avatar
+          class="mr-1"
+          color="grey darken-1"
+          size="38"
+        ></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+       
+
+        <v-spacer></v-spacer>
+ 
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+      
+    </v-app-bar>
+
+    <div class="home">
+          <Mapa/>
+      </div>
+  </v-app>
+  
+</template>
+
+<script>
+import Mapa from "./components/Mapa"
+export default {
+  components: { Mapa },
+  data: () => ({
+    links: [
+      "Seja um RND",
+      "Sugestões",
+      "Sobre",
+      "Compartilhe a sua experiência",
+    ],
+  }),
+};
+</script>
---
+++ src/components/Mapa.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col cols="2">
+          <v-sheet rounded="lg">
+            <v-card class="mx-auto" max-width="500">
+              <v-list>
+                <v-list-item-group v-model="model">
+                  <v-list-item v-for="(item, i) in items" :key="i">
+                    <v-list-item-icon>
+                      <v-icon v-text="item.icon"></v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-sheet>
+        </v-col>
+
+        <v-col>
+          <v-sheet min-height="70vh" rounded="lg">
+            id = map
+       
+         </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<style>
+ #map {
+    height: 50%;
+  }
+</style>
+
+<script>
+export default {
+   data: () => ({
+      items: [
+        {
+          icon: 'mdi-inbox',
+          text: 'Gravíssimos',
+        },
+        {
+          icon: 'mdi-star',
+          text: 'Grave',
+        },
+        {
+          icon: 'mdi-send',
+          text: 'Mediano',
+        },
+        {
+          icon: 'mdi-email-open',
+          text: 'Alertas',
+        },
+      ],
+      model: 1,
+    }),
+   }
+</script>
